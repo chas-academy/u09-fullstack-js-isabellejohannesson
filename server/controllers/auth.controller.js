8457d945a953688b9941e8a23d810e7362c40da9
Isabelle Johannesson <isabelle.johannesson@outlook.com>
2024-09-26 09:30:45 +0200

    mvc setup and db connection

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+    res.json({
+        data: "This is signup"
+    });
+};
+
+export const login = async (req, res) => {
+    res.json({
+        data: "This is login"
+    });
+}
+
+export const logout = async (req, res) => {
+    res.json({
+        data: "This is log out"
+    });
+}
