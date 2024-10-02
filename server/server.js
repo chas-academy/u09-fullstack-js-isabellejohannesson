cf4ecaa23716e930bf641ff0fb34bbcf418838c2
Isabelle Johannesson <isabelle.johannesson@outlook.com>
2024-10-02 13:57:57 +0200

    setup och databasconnection klar

--- server/server.js
+++ server/server.js
@@ -6,12 +6,11 @@ import connectMongoDB from "./db/connectMongoDB.js";
 dotenv.config();
 
 const app = express();
-
-console.log(process.env.MONGO_URI);
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(8000, () => {
-    console.log("Server is running on port 8000");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
     connectMongoDB();
 })
