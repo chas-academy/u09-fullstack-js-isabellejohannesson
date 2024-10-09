c9c3c9d812aeb1dfad6e9db8997ae33d91078c7a
Isabelle Johannesson <isabelle.johannesson@outlook.com>
2024-10-09 11:18:56 +0200

    update user funkar, testad med Insomnia

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,13 +1,12 @@
 import express from "express";
 import { protectedRoute } from "../middleware/protectedRoute.js";
-import { getUserProfile } from "../controllers/user.controller.js";
-import { followUnfollowUser } from "../controllers/user.controller.js";
+import { getSuggestedUsers, getUserProfile, followUnfollowUser, updateUserProfile } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/profile/:userName", protectedRoute, getUserProfile)
-/* router.get("/suggested", protectedRoute, getUserProfile) */
+router.get("/suggested", protectedRoute, getSuggestedUsers)
 router.post("/follow/:id", protectedRoute, followUnfollowUser)
-/* router.post("/update", protectedRoute, updateUserProfile) */
+router.post("/update", protectedRoute, updateUserProfile) 
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.routes.js";
+import {v2 as cloudinary} from 'cloudinary';
 
 dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // parse form data from user input
+app.use(express.urlencoded({extended: true}));
 
 app.use(cookieParser());
 
