e81b4bce887ea72dca5a4bbfa6cc788bae4c1c68
Isabelle Johannesson <isabelle.johannesson@outlook.com>
2024-10-10 11:58:34 +0200

    get all posts, get following, get likes klara

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { protectedRoute } from '../middleware/protectedRoute.js';
-import { createPost, deletePost, commentPost, likeUnlikePost } from '../controllers/post.controller.js';
+import { createPost, deletePost, commentPost, likeUnlikePost, getAllPosts, getLikedPosts,  getFollowingPosts, getUserPosts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
+router.get("/all", protectedRoute, getAllPosts)
+router.get("/following", protectedRoute, getFollowingPosts)
+router.get("/likes/:id", protectedRoute, getLikedPosts)
+router.get("/user/:userName", protectedRoute, getUserPosts)
 router.post("/create", protectedRoute, createPost)
 router.post("/like/:id", protectedRoute, likeUnlikePost)
 router.post("/comment/:id", protectedRoute, commentPost)
