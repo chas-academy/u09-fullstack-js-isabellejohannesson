74a3fe69e1875342ee763e4df3f997e9135f6b0d
Isabelle Johannesson <isabelle.johannesson@outlook.com>
2024-10-23 13:27:44 +0200

    sänkte gränsen för payload till 5mb på server pga risk för DOS-attacker

--- server/server.js
+++ server/server.js
@@ -21,8 +21,8 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json({limit: "50mb"})); 
-app.use(express.urlencoded({extended: true, limit: "50mb"}));
+app.use(express.json({limit: "5mb"})); 
+app.use(express.urlencoded({extended: true, limit: "5mb"}));
 
 
 app.use(cookieParser());
